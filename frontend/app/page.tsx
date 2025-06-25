bdcf16d161a69d8e644f23f3e9849d78a45efa7c
rayaran1000 <aranya.ray1998@gmail.com>
2025-06-25 15:49:03 +0530

    Frontend setup done

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Test() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Tailwind is Working!</h1>
+    </div>
+  );
+}
